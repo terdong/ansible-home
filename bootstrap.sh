8d0dd43a25b66fe4bcb9f18bde38a1c63a5f2f57
Darren Kim <terdong@gmail.com>
2026-06-22 23:12:48 +0900

    feat: add bootstrap script for sudoers configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+USER_ID=$(whoami)
+
+if [ ! -f "/etc/sudoers.d/$USER_ID" ]; then
+    echo "Configuring sudoers for $USER_ID..."
+    echo "$USER_ID ALL=(ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/$USER_ID" > /dev/null
+    sudo chmod 0440 "/etc/sudoers.d/$USER_ID"
+    echo "Sudoers configuration complete."
+else
+    echo "Sudoers configuration already exists for $USER_ID."
+fi
+
 sudo apt update
 
 sudo apt install -y ansible
